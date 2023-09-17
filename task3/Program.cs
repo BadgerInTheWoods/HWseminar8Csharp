b46e559b252390a924a6a0dc57df6b49a1c867d1
AlexanderBadger <sashazateev444@gmail.com>
2023-09-17 16:26:57 +0200

    minor corrections

--- task3/Program.cs
+++ task3/Program.cs
@@ -2,11 +2,11 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 Console.WriteLine("insert first number");
-double M = Convert.ToDouble(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("insert second number");
-double N = Convert.ToDouble(Console.ReadLine());
+int M = Convert.ToInt32(Console.ReadLine());
 
-double ack(double m, double n)
+int ack(int n, int m)
 {
     while (n != 0)
     {
@@ -22,4 +22,5 @@ double ack(double m, double n)
     }
     return m + 1;
 }
-Console.WriteLine(ack(M, N));
+Console.WriteLine(ack(N, M));
+
